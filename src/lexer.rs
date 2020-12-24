cff293f4473cb2e0274f48b7462aba2cc07bb755
Hector Peeters <hector.peeters@gmail.com>
2020-12-24 18:14:10 +0100

    Add lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -33,9 +33,8 @@ pub fn lex(input: &str) -> DynoResult<Vec<Token>> {
 
     let mut tokens: Vec<Token> = vec![];
 
-    let mut token;
     loop {
-        token = lex.next();
+        let token = lex.next();
 
         if token.is_none() {
             break;
@@ -52,4 +51,45 @@ pub fn lex(input: &str) -> DynoResult<Vec<Token>> {
     }
 
     return Ok(tokens);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Token::*;
+    use super::*;
+
+    fn get_tokens(input: &str) -> Vec<Token> {
+        let tokens = lex(input);
+        assert!(tokens.is_ok());
+        tokens.unwrap()
+    }
+
+    #[test]
+    fn lexer_empty() {
+        let tokens = get_tokens("");
+        assert_eq!(tokens.len(), 0);
+    }
+
+    #[test]
+    fn lexer_integer_literal() {
+        let tokens = get_tokens("12 0 439394474 123");
+
+        assert_eq!(tokens[0], IntegerLiteral("12".to_string()));
+        assert_eq!(tokens[1], Whitespace);
+        assert_eq!(tokens[2], IntegerLiteral("0".to_string()));
+        assert_eq!(tokens[3], Whitespace);
+        assert_eq!(tokens[4], IntegerLiteral("439394474".to_string()));
+        assert_eq!(tokens[5], Whitespace);
+        assert_eq!(tokens[6], IntegerLiteral("123".to_string()));
+    }
+
+    #[test]
+    fn lexer_binary_operands() {
+        let tokens = get_tokens("+-*/");
+
+        assert_eq!(tokens[0], Plus);
+        assert_eq!(tokens[1], Minus);
+        assert_eq!(tokens[2], Asterix);
+        assert_eq!(tokens[3], Slash);
+    }
 }
