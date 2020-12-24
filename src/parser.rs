3213db285199e7e5deeaaff8eb23faea3342f320
Hector Peeters <hector.peeters@gmail.com>
2020-12-24 18:26:40 +0100

    Add basic parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -46,3 +46,60 @@ impl Parser {
 pub fn parse(input: Vec<Token>) -> DynoResult<AstNode> {
     Ok(AstNode::Empty())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::AstNode::*;
+    use crate::ast::BinaryOperation::*;
+    use crate::lexer::Token::*;
+
+    #[test]
+    fn parser_new() {
+        let parser = Parser::new(vec![]);
+
+        assert!(parser.is_eof());
+    }
+
+    #[test]
+    fn parser_peek() {
+        let parser = Parser::new(vec![Plus, Whitespace, Minus]);
+
+        let token = parser.peek();
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Plus);
+
+        let token = parser.peek_next(0);
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Plus);
+
+        let token = parser.peek_next(1);
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Whitespace);
+
+        let token = parser.peek_next(2);
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Minus);
+
+        assert!(parser.peek_next(3).is_err());
+    }
+
+    #[test]
+    fn parser_consume() {
+        let mut parser = Parser::new(vec![Plus, Whitespace, Minus]);
+
+        let token = parser.consume();
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Plus);
+
+        let token = parser.consume();
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Whitespace);
+
+        let token = parser.consume();
+        assert!(token.is_ok());
+        assert_eq!(*token.unwrap(), Minus);
+
+        assert!(parser.consume().is_err());
+    }
+}
