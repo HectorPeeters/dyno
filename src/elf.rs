6cba629c37ad8b9fbef5c6736a7f600600371c99
Hector Peeters <hector.peeters@gmail.com>
2020-12-27 12:24:40 +0100

    Move elf write test from main.rs to elf.rs

--- src/elf.rs
+++ src/elf.rs
@@ -348,3 +348,58 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_elf_write() {
+        let mut writer = std::io::BufWriter::new(vec![]);
+
+        let elf_file = ElfFileInfo {
+            program_header_table: vec![ElfProgramHeaderEntry {
+                segment_type: ElfProgramHeaderEntryType::PtLoad,
+                flags: ELF_PROGRAM_FLAG_READ | ELF_PROGRAM_FLAG_EXECUTE,
+                offset: 0x00,
+                virtual_address: 0x400000,
+                physical_address: 0x400000,
+                file_size: 0x8C,
+                memory_size: 0x8C,
+                align: 0x200000,
+            }],
+            section_header_table: vec![
+                NULL_SECTION,
+                ElfSectionHeaderEntry {
+                    name: ".text".to_string(),
+                    section_type: ElfSectionType::ShtProgBits,
+                    flags: ELF_SECTION_FLAG_ALLOC | ELF_SECTION_FLAG_EXECINSTR,
+                    address: 0x400080,
+                    offset: 0x80,
+                    size: 0x0C,
+                    link: 0x00,
+                    info: 0x00,
+                    address_align: 0x10,
+                    entry_size: 0x00,
+                },
+                ElfSectionHeaderEntry {
+                    name: ".shstrtab".to_string(),
+                    section_type: ElfSectionType::ShtStrTab,
+                    flags: 0x00,
+                    address: 0x00,
+                    offset: 0x8C,
+                    size: 0x11,
+                    link: 0x00,
+                    info: 0x00,
+                    address_align: 0x01,
+                    entry_size: 0x00,
+                },
+            ],
+            code: vec![
+                0xB8, 0x01, 0x00, 0x00, 0x00, 0xBB, 0x2A, 0x00, 0x00, 0x00, 0xCD, 0x80,
+            ],
+        };
+
+        write_elf_file(&mut writer, &elf_file).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,11 @@ mod ast;
 mod elf;
 mod error;
 mod generator;
+mod jit;
 mod lexer;
 mod parser;
 mod types;
 
 fn main() -> error::DynoResult<()> {
-    let mut writer = std::fs::File::create("test.out").unwrap();
-
-    let elf_file = elf::ElfFileInfo {
-        program_header_table: vec![elf::ElfProgramHeaderEntry {
-            segment_type: elf::ElfProgramHeaderEntryType::PtLoad,
-            flags: elf::ELF_PROGRAM_FLAG_READ | elf::ELF_PROGRAM_FLAG_EXECUTE,
-            offset: 0x00,
-            virtual_address: 0x400000,
-            physical_address: 0x400000,
-            file_size: 0x8C,
-            memory_size: 0x8C,
-            align: 0x200000,
-        }],
-        section_header_table: vec![
-            elf::NULL_SECTION,
-            elf::ElfSectionHeaderEntry {
-                name: ".text".to_string(),
-                section_type: elf::ElfSectionType::ShtProgBits,
-                flags: elf::ELF_SECTION_FLAG_ALLOC | elf::ELF_SECTION_FLAG_EXECINSTR,
-                address: 0x400080,
-                offset: 0x80,
-                size: 0x0C,
-                link: 0x00,
-                info: 0x00,
-                address_align: 0x10,
-                entry_size: 0x00,
-            },
-            elf::ElfSectionHeaderEntry {
-                name: ".shstrtab".to_string(),
-                section_type: elf::ElfSectionType::ShtStrTab,
-                flags: 0x00,
-                address: 0x00,
-                offset: 0x8C,
-                size: 0x11,
-                link: 0x00,
-                info: 0x00,
-                address_align: 0x01,
-                entry_size: 0x00,
-            },
-        ],
-        code: vec![
-            0xB8, 0x01, 0x00, 0x00, 0x00, 0xBB, 0x2A, 0x00, 0x00, 0x00, 0xCD, 0x80,
-        ],
-    };
-
-    elf::write_elf_file(&mut writer, &elf_file)?;
-
     Ok(())
 }
