d0a70d5120cf287bde428b17884593430062abb5
Hector Peeters <hector.peeters@gmail.com>
2020-12-27 13:24:09 +0100

    Change X86 generator structure

--- src/generator.rs
+++ src/generator.rs
@@ -1,13 +1,41 @@
 use crate::ast::AstNode;
+use std::io::BufWriter;
 
-pub trait Generator {
-    fn gen_assembly(ast: AstNode) -> Vec<String>;
+struct X86Generator {
+    writer: BufWriter<Vec<u8>>,
+    ast: AstNode,
 }
 
-pub struct X86Generator;
+impl X86Generator {
+    fn new(ast: AstNode) -> Self {
+        Self {
+            writer: BufWriter::new(vec![]),
+            ast,
+        }
+    }
 
-impl Generator for X86Generator {
-    fn gen_assembly(ast: AstNode) -> Vec<String> {
+    fn gen(&self) -> Vec<u8> {
         vec![]
+    }
+}
+
+pub fn gen_assembly(ast: AstNode) -> Vec<u8> {
+    let generator = X86Generator::new(ast);
+    generator.gen()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lexer::lex;
+    use crate::parser::parse;
+
+    fn get_generator(input: &str) -> X86Generator {
+        X86Generator::new(parse(lex(input).unwrap()).unwrap())
+    }
+
+    #[test]
+    fn generator_new() {
+        let _ = get_generator("1 + 12");
     }
 }
