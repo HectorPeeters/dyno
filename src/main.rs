65ac1ee996c11845036de20986f5885e56383348
Hector Peeters <hector.peeters@gmail.com>
2020-12-27 17:01:16 +0100

    Add basic REPL

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,49 @@ mod lexer;
 mod parser;
 mod types;
 
-fn main() -> error::DynoResult<()> {
-    Ok(())
+use std::io::{stdin, stdout, Write};
+
+fn read_input() -> String {
+    let mut input = String::new();
+
+    print!("> ");
+
+    let _ = stdout().flush();
+
+    stdin()
+        .read_line(&mut input)
+        .expect("Did not enter a correct input");
+
+    input
+}
+
+fn main() {
+    loop {
+        let input = read_input();
+
+        let tokens = lexer::lex(&input);
+        if tokens.is_err() {
+            eprintln!("Failed to tokenize input: {:?}", tokens.err());
+            continue;
+        }
+        let tokens = tokens.unwrap();
+
+        let ast = parser::parse(tokens);
+        if ast.is_err() {
+            eprintln!("Failed to create ast: {:?}", ast.err());
+            continue;
+        }
+        let ast = ast.unwrap();
+
+        let assembly = generator::gen_assembly(ast);
+        if assembly.is_err() {
+            eprintln!("Failed to generate assembly: {:?}", assembly.err());
+            continue;
+        }
+        let assembly = assembly.unwrap();
+
+        let jit = jit::Jit::new(&assembly);
+        let result = jit.run();
+        println!("=> {}", result);
+    }
 }
