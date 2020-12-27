aefe28be2ad390dfb9d557404555310d3f688dfe
Hector Peeters <hector.peeters@gmail.com>
2020-12-27 14:01:20 +0100

    Rename push_* to write_* in x86 generator

--- src/generator.rs
+++ src/generator.rs
@@ -23,15 +23,15 @@ impl X86Generator {
         }
     }
 
-    fn push_u8(&mut self, data: u8) -> DynoResult<()> {
+    fn write_u8(&mut self, data: u8) -> DynoResult<()> {
         self.write(&[data])
     }
 
-    fn push_u16(&mut self, data: u16) -> DynoResult<()> {
+    fn write_u16(&mut self, data: u16) -> DynoResult<()> {
         self.write(&[((data >> 0) & 0xFF) as u8, ((data >> 8) & 0xFF) as u8])
     }
 
-    fn push_u32(&mut self, data: u32) -> DynoResult<()> {
+    fn write_u32(&mut self, data: u32) -> DynoResult<()> {
         self.write(&[
             ((data >> 0) & 0xFF) as u8,
             ((data >> 8) & 0xFF) as u8,
@@ -40,7 +40,7 @@ impl X86Generator {
         ])
     }
 
-    fn push_u64(&mut self, data: u64) -> DynoResult<()> {
+    fn write_u64(&mut self, data: u64) -> DynoResult<()> {
         self.write(&[
             ((data >> 0) & 0xFF) as u8,
             ((data >> 8) & 0xFF) as u8,
@@ -53,13 +53,25 @@ impl X86Generator {
         ])
     }
 
-    fn gen(&self) -> DynoResult<Vec<u8>> {
+    fn write_prologue(&mut self) -> DynoResult<()> {
+        self.write(&[0x55, 0x48, 0x89, 0xE5])?;
+        Ok(())
+    }
+
+    fn write_epilogue(&mut self) -> DynoResult<()> {
+        self.write(&[0x48, 0x89, 0xE5, 0x5D, 0xC3]);
+        Ok(())
+    }
+
+    fn gen(&mut self) -> DynoResult<Vec<u8>> {
+        self.write_prologue();
+        self.write_epilogue();
         Ok(self.writer.buffer().to_vec())
     }
 }
 
 pub fn gen_assembly(ast: AstNode) -> DynoResult<Vec<u8>> {
-    let generator = X86Generator::new(ast);
+    let mut generator = X86Generator::new(ast);
     generator.gen()
 }
 
@@ -79,34 +91,34 @@ mod tests {
     }
 
     #[test]
-    fn generator_push_u8() {
+    fn generator_write_u8() {
         let mut generator = get_generator("");
 
-        generator.push_u8(12).unwrap();
+        generator.write_u8(12).unwrap();
         assert_eq!(generator.writer.buffer(), &[12]);
     }
 
     #[test]
-    fn generator_push_u16() {
+    fn generator_write_u16() {
         let mut generator = get_generator("");
 
-        generator.push_u16(0x1234).unwrap();
+        generator.write_u16(0x1234).unwrap();
         assert_eq!(generator.writer.buffer(), &[0x34, 0x12]);
     }
 
     #[test]
-    fn generator_push_u32() {
+    fn generator_write_u32() {
         let mut generator = get_generator("");
 
-        generator.push_u32(0x12345678).unwrap();
+        generator.write_u32(0x12345678).unwrap();
         assert_eq!(generator.writer.buffer(), &[0x78, 0x56, 0x34, 0x12]);
     }
 
     #[test]
-    fn generator_push_u64() {
+    fn generator_write_u64() {
         let mut generator = get_generator("");
 
-        generator.push_u64(0x1234567812345678).unwrap();
+        generator.write_u64(0x1234567812345678).unwrap();
         assert_eq!(
             generator.writer.buffer(),
             &[0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12]
