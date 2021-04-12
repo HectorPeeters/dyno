2766d970695a7a9073187521a98928e1d1bdaae7
Hector Peeters <hector.peeters@gmail.com>
2021-04-12 16:09:17 +0200

    Add integer literal jit compilation

--- src/generator.rs
+++ src/generator.rs
@@ -1,10 +1,14 @@
-use crate::ast::{AstNode, BinaryOperationType};
+use crate::ast::AstNode;
 use crate::error::*;
+use crate::types::*;
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::execution_engine::{ExecutionEngine, JitFunction};
 use inkwell::module::Module;
+use inkwell::values::BasicValueEnum;
 use inkwell::OptimizationLevel;
+
+type MainFunc = unsafe extern "C" fn() -> u64;
 
 pub struct CodeGenerator<'a> {
     context: &'a Context,
@@ -14,8 +18,53 @@ pub struct CodeGenerator<'a> {
 }
 
 impl CodeGenerator<'_> {
+    fn generate_literal(
+        &self,
+        literal_type: &DynoType,
+        value: &DynoValue,
+    ) -> DynoResult<BasicValueEnum> {
+        match (literal_type, value) {
+            (DynoType::UInt8(), DynoValue::UInt(x)) => Ok(BasicValueEnum::IntValue(
+                self.context.i8_type().const_int(*x, false),
+            )),
+            _ => Err(DynoError::GeneratorError(format!(
+                "Invalid type-value pair: {:?} {:?}",
+                literal_type, value
+            ))),
+        }
+    }
+
+    fn generate(&self, ast: &AstNode) -> DynoResult<BasicValueEnum> {
+        match ast {
+            AstNode::Literal(literal_type, value) => self.generate_literal(&literal_type, &value),
+            AstNode::Return(x) => self.generate(x),
+            _ => Err(DynoError::GeneratorError(format!(
+                "Unknown ast value: {:?}",
+                ast
+            ))),
+        }
+    }
+
     pub fn jit_execute(&self, ast: &AstNode) -> DynoResult<u64> {
-        Ok(0)
+        let value = self.generate(ast)?;
+
+        let i64_type = self.context.i64_type();
+        let fn_type = i64_type.fn_type(&[], false);
+        let function = self.module.add_function("main", fn_type, None);
+        let basic_block = self.context.append_basic_block(function, "entry");
+
+        self.builder.position_at_end(basic_block);
+        let return_value =
+            self.builder
+                .build_int_s_extend(value.into_int_value(), i64_type, "");
+        self.builder.build_return(Some(&return_value));
+
+        unsafe {
+            let function: JitFunction<MainFunc> =
+                self.execution_engine.get_function("main").unwrap();
+
+            Ok(function.call())
+        }
     }
 }
 
